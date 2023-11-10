864ad11933041b6dccf1589b64b941f44745f6f2
Layla <layla.kawafi@hotmail.com>
2023-11-10 11:46:15 +0000

    job card horizontal scroll when clicking on postcode

--- ElderHelperReactNative/Components/JobsMap.js
+++ ElderHelperReactNative/Components/JobsMap.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Polygon } from 'react-native-maps';
-import ManchesterPostcodes from '../assets/ManchesterPostcodes.json';
-import * as Location from 'expo-location';
-import { useState, useEffect } from 'react';
-import JobCard from './JobCard';
-
-
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Polygon } from "react-native-maps";
+import ManchesterPostcodes from "../assets/ManchesterPostcodes.json";
+import * as Location from "expo-location";
+import { useState, useEffect } from "react";
+import JobCard from "./JobCard";
 
 export default function JobsMap() {
- const [errorMsg, setErrorMsg] = useState(null);
- const [location, setLocation] = useState(null);
-   
- useEffect(() => {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
     (async () => {
-      
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log(status)
-        setErrorMsg('Permission to access location was denied');
+      setErrorMsg(null);
+      if (status !== "granted") {
+        console.log(status);
+        setErrorMsg("Permission to access location was denied");
+        setErrorMsg(null);
         return;
       }
 
@@ -27,75 +26,80 @@ export default function JobsMap() {
     })();
   }, []);
 
-  let text = 'Waiting..';
+  let text = "Waiting..";
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
-    text = `latitude:${JSON.stringify(location.latitude)}, longitude:${JSON.stringify(location.longitude)}`;
+    text = `latitude:${JSON.stringify(
+      location.latitude
+    )}, longitude:${JSON.stringify(location.longitude)}`;
   }
-    const handlePress = (title) => {    
-        setPress(title)
-        console.log(title)
-    }
-const [press, setPress] = useState('')
-   
-    return (
+
+  const handlePress = (title) => {
+    setPress(title);
+    console.log(title);
+  };
+  const [press, setPress] = useState("");
+
+  return (
     <View style={styles.view}>
-    <Text style={styles.text}>{text}</Text>
-    <JobCard  press={press} setPress={setPress}/>
-    <MapView 
+      <Text style={styles.text}>{text}</Text>
+      <MapView
         initialRegion={{
-            latitude: 53.4808,
-            longitude: -2.2926,
-            latitudeDelta: 0.43,
-            longitudeDelta: 0.34
+          latitude: 53.4808,
+          longitude: -2.2926,
+          latitudeDelta: 0.43,
+          longitudeDelta: 0.34,
         }}
         style={styles.map}
-        provider={PROVIDER_GOOGLE} 
-        showsUserLocation={true} 
-        onUserLocationChange={(event)=> {setLocation(event.nativeEvent.coordinate)}}
-    >
-    {
-        ManchesterPostcodes.features.map((area) => {
-            const coordinates = area.geometry.coordinates[0].map(([longitude, latitude]) => ({
-                latitude,
-                longitude,
-            }));
-            return (
-                <Polygon
-                    tappable
-                    key={area.properties.name}
-                    coordinates={coordinates}
-                    fillColor="rgba(0, 200, 0, 0.5)"
-                    strokeColor="rgba(0,0,0,0.5)"
-                    strokeWidth={2}
-                    onPress= {(event) => { 
-                        handlePress(event._targetInst.return.key)
-                    }}
-                />
-            )
-        })
-    }
-    
-    </MapView>    
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        onUserLocationChange={(event) => {
+          setLocation(event.nativeEvent.coordinate);
+        }}
+      >
+        {ManchesterPostcodes.features.map((area) => {
+          const coordinates = area.geometry.coordinates[0].map(
+            ([longitude, latitude]) => ({
+              latitude,
+              longitude,
+            })
+          );
+          return (
+            <Polygon
+              tappable
+              key={area.properties.name}
+              coordinates={coordinates}
+              fillColor="rgba(0, 200, 0, 0.5)"
+              strokeColor="rgba(0,0,0,0.5)"
+              strokeWidth={2}
+              onPress={(event) => {
+                handlePress(event._targetInst.return.key);
+              }}
+            />
+          );
+        })}
+      </MapView>
+
+      <JobCard press={press} setPress={setPress} />
     </View>
-    )
+  );
 }
 
-const styles = StyleSheet.create({ 
-    map: {
-        flex: 4 
-    },
-    
-    text: {
-        flex: 1,
-        fontSize: 30,
-        color: '#66B2FF',
-        textAlign: 'center',
-        marginTop: 30
-    },
-    view: {
-        height: '100%',
-        width: '100%'
-    }
-})
+const styles = StyleSheet.create({
+  map: {
+    flex: 4,
+  },
+
+  text: {
+    flex: 1,
+    fontSize: 30,
+    color: "#66B2FF",
+    textAlign: "center",
+    marginTop: 30,
+  },
+  view: {
+    height: "100%",
+    width: "100%",
+  },
+});
